4b5b31998ce53b4d82c9cbd1448070a11d7d5e30
Ch Ch <kristian-chernov2013@yandex.ru>
2024-04-24 17:04:56 +0500

    Add file json, add differ

--- src/Differ.php
+++ src/Differ.php
@@ -1,1 +1,16 @@
 <?php
+
+namespace Differ\Differ;
+
+function genDiff(array $pathToFile1, array $pathToFile2): string
+{
+    $decode1 = json_decode($pathToFile1, true);
+    $decode2 = json_decode($pathToFile2, true);
+
+    $keys = array_merge(array_keys($pathToFile1), array_keys($pathToFile2));
+    sort($keys);
+    $result = [];
+
+
+    }
+}
