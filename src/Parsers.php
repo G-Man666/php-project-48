9bdcdf77299340597e45c95e13a443031cf37f51
Ch Ch <kristian-chernov2013@yandex.ru>
2024-05-16 16:57:21 +0500

    Revork

--- src/Parsers.php
+++ src/Parsers.php
@@ -6,12 +6,12 @@ use Symfony\Component\Yaml\Yaml;
 
 function parse(string $content, string $extension): object
 {
-    switch ($extension) {
-        case 'json':
-            return json_decode($content);
-        case 'yaml':
-        case 'yml':
-            return Yaml::parse($content, Yaml::PARSE_OBJECT_FOR_MAP);
-        default:
-            throw new \Exception('Wrong type of file');
-    }
+switch ($extension) {
+    case 'json':
+        return json_decode($content);
+    case 'yaml':
+    case 'yml':
+        return Yaml::parse($content, Yaml::PARSE_OBJECT_FOR_MAP);
+    default:
+        throw new \Exception('Wrong type of file');
+}
---
+++ tests/DifferTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Differ\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+use function Differ\Differ\genDiff;
+
+class DifferTest extends TestCase
+{
+    public function testGenerateDiffJson(): void
+    {
+        $expected = file_get_contents('./tests/fixtures/nestedStylishDiff');
+        $actual = genDiff();
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testGenereateDiffYaml(): void
+    {
+        $expected = file_get_contents('./tests/fixtures/nestedStylishDiff');
+        $actual = genDiff('./tests/fixtures/file1.yaml', './tests/fixtures/file2.yml');
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testgenDiffNestedPlainJson(): void
+    {
+        $expected = file_get_contents('./tests/fixtures/nestedPlainDiff');
+        $actual = genDiff('./tests/fixtures/file1.json', './tests/fixtures/file2.json', 'plain');
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testgenDiffNestedPlainYaml(): void
+    {
+        $expected = file_get_contents('./tests/fixtures/nestedPlainDiff');
+        $actual = genDiff('./tests/fixtures/file1.yaml', './tests/fixtures/file2.yml', 'plain');
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testgenDiffNestedJsonJson(): void
+    {
+        $expected = file_get_contents('./tests/fixtures/nestedJsonDiff');
+        $actual = genDiff('./tests/fixtures/file1.json', './tests/fixtures/file2.json', 'json');
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testgenDiffNestedJsonYaml(): void
+    {
+        $expected = file_get_contents('./tests/fixtures/nestedJsonDiff');
+        $actual = genDiff('./tests/fixtures/file1.yaml', './tests/fixtures/file2.yml', 'json');
+        $this->assertEquals($expected, $actual);
+    }
+}
